be0792ea92019fbaf9533ac8e18efe74a85dc7eb
Kimminhyeo <ualsgur99@gmail.com>
2023-03-26 15:42:16 +0900

    댓글 관리 서비스의 비즈니스 로직 및 컨트롤러 테스트 작성

---
+++ src/main/java/com/min/projectboardadmin/dto/security/ArticleCommentDto.java
@@ -0,0 +1,38 @@
+package com.min.projectboardadmin.dto.security;
+
+import com.min.projectboardadmin.dto.UserAccountDto;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ArticleCommentDto {
+
+    Long id;
+    Long articleId;
+    UserAccountDto userAccount;
+    Long parentCommentId;
+    String content;
+    LocalDateTime createdAt;
+    String createdBy;
+    LocalDateTime modifiedAt;
+    String modifiedBy;
+
+    private ArticleCommentDto(){}
+
+    protected ArticleCommentDto(Long id, Long articleId, UserAccountDto userAccount, Long parentCommentId, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        this.id = id;
+        this.articleId = articleId;
+        this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.modifiedAt = modifiedAt;
+        this.modifiedBy = modifiedBy;
+    }
+
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, Long parentCommentId, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, parentCommentId, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+}

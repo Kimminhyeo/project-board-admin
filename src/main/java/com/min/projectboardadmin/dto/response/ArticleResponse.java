bcbf5320b50ff433a6d37045f179622fd7efb749
Kimminhyeo <ualsgur99@gmail.com>
2023-03-27 23:08:58 +0900

    게시글 관리 페이지 구현: 컨트롤러, 서비스, 뷰

---
+++ src/main/java/com/min/projectboardadmin/dto/response/ArticleResponse.java
@@ -0,0 +1,39 @@
+package com.min.projectboardadmin.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.min.projectboardadmin.dto.ArticleDto;
+import com.min.projectboardadmin.dto.UserAccountDto;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ArticleResponse {
+
+    Long id;
+    UserAccountDto userAccount;
+    String title;
+    String content;
+    LocalDateTime createdAt;
+
+    private ArticleResponse(){}
+
+    public ArticleResponse(Long id, UserAccountDto userAccount, String title, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.userAccount = userAccount;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public static ArticleResponse of(Long id, UserAccountDto userAccount, String title, String content, LocalDateTime createdAt) {
+        return new ArticleResponse(id, userAccount, title, content, createdAt);
+    }
+
+    public static ArticleResponse withContent(ArticleDto dto) {
+        return ArticleResponse.of(dto.getId(), dto.getUserAccount(), dto.getTitle(), dto.getContent(), dto.getCreatedAt());
+    }
+
+    public static ArticleResponse withoutContent(ArticleDto dto) {
+        return ArticleResponse.of(dto.getId(), dto.getUserAccount(), dto.getTitle(), null, dto.getCreatedAt());
+    }
+}

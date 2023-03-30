5f5fb6e8cb23641bdd4e7157ef34210018444727
Kimminhyeo <ualsgur99@gmail.com>
2023-03-30 18:52:19 +0900

    테스트 수정

--- src/test/java/com/min/projectboardadmin/service/UserAccountManagementServiceTest.java
+++ src/test/java/com/min/projectboardadmin/service/UserAccountManagementServiceTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.min.projectboardadmin.dto.UserAccountDto;
 import com.min.projectboardadmin.dto.properties.ProjectProperties;
 import com.min.projectboardadmin.dto.response.UserAccountClientResponse;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -29,7 +30,7 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 @DisplayName("비즈니스 로직 - 회원 관리")
 class UserAccountManagementServiceTest {
 
-    //    @Disabled("실제 API 호출 결과 관찰용이므로 평상시엔 비활성화")
+    @Disabled("실제 API 호출 결과 관찰용이므로 평상시엔 비활성화")
     @DisplayName("실제 API 호출 테스트")
     @SpringBootTest
     @Nested
